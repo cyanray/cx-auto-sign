ece1ebb1021476f25b4f54017c8c1416d2f3c1bf
cyanray <a484599279@hotmail.com>
2020-08-29 03:27:55 +0800

    更新: LoginObject 同时适配两个登录接口;

--- CxSignHelper/Models/LoginObject.cs
+++ CxSignHelper/Models/LoginObject.cs
@@ -1,14 +1,29 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace CxSignHelper.Models
 {
     internal class LoginObject
     {
-        [JsonProperty("mes")]
         public string Message { get; set; }
 
         [JsonProperty("status")]
         public bool Status { get; set; }
+
+        [JsonProperty("mes")]
+        private string _message1 
+        {
+            get => Message;
+            set => Message = value;
+        }
+
+        [JsonProperty("msg")]
+        private string _message2
+        {
+            get => Message;
+            set => Message = value;
+        }
+
     }
 
 }
