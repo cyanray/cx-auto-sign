1bc0eba042e91dea15655a4c16055a5118ca30e5
cyanray <a484599279@hotmail.com>
2020-08-29 09:27:38 +0800

    新增: CommandBase 可自动读取保存AppConfig;

--- cx-auto-sign/CommandBase.cs
+++ cx-auto-sign/CommandBase.cs
@@ -1,4 +1,8 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using cx_auto_sign.Models;
+using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace cx_auto_sign
@@ -6,6 +10,35 @@ namespace cx_auto_sign
     [HelpOption("-h|--help")]
     abstract class CommandBase
     {
+        protected const string AppConfigPath = "AppConfig.json";
+
+        private static AppConfig _appConfig = null;
+
+        protected static AppConfig AppConfig 
+        {
+            get
+            {
+                if (_appConfig is null) LoadAppConfig();
+                return _appConfig;
+            }
+            set
+            {
+                _appConfig = value;
+                SaveAppConfig();
+            }
+        }
+
+        protected static void LoadAppConfig()
+        {
+            if (!File.Exists(AppConfigPath)) return;
+            var text = File.ReadAllText(AppConfigPath);
+            _appConfig = JsonConvert.DeserializeObject<AppConfig>(text);
+        }
+
+        protected static void SaveAppConfig()
+        {
+            File.WriteAllText(AppConfigPath, JsonConvert.SerializeObject(_appConfig));
+        }
 
         protected virtual Task<int> OnExecuteAsync(CommandLineApplication app)
         {
