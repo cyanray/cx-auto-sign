a69682d5cb899a91e7464c9100de859d35be5d6c
cyanray <a484599279@hotmail.com>
2020-08-29 09:24:33 +0800

    新增: AppConfig类;

---
+++ cx-auto-sign/Models/AppConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cx_auto_sign.Models
+{
+    public class AppConfig
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fid { get; set; } = null;
+        public string Address { get; set; } = "中国";
+        public string Latitude { get; set; } = "-1";
+        public string Longitude { get; set; } = "-1";
+        public string ClientIp { get; set; } = "1.1.1.1";
+    }
+}
