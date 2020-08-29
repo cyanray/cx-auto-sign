af46bed3a18cb2740cad83f32a07676b9dabd00b
cyanray <a484599279@hotmail.com>
2020-08-29 09:52:27 +0800

    新增: CourseModel;

---
+++ CxSignHelper/Models/CourseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CxSignHelper.Models
+{
+    public class CourseModel
+    {
+        public string CourseId { get; set; }
+        public string ClassId { get; set; }
+    }
+}
