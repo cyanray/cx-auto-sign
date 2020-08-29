e75442fec984bf4feb5cd89eaab5739c2594b071
cyanray <a484599279@hotmail.com>
2020-08-29 09:29:38 +0800

    更新: work 指令监控消息事件的大致流程;

--- cx-auto-sign/WorkCommand.cs
+++ cx-auto-sign/WorkCommand.cs
@@ -1,6 +1,12 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using CxSignHelper;
+using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Websocket.Client;
 
 namespace cx_auto_sign
 {
@@ -8,20 +14,85 @@ namespace cx_auto_sign
     class WorkCommand : CommandBase
     {
 
-        protected override Task<int> OnExecuteAsync(CommandLineApplication app)
+        protected override async Task<int> OnExecuteAsync(CommandLineApplication app)
         {
             // 读取总配置
 
             // 读取所有课程配置
 
-            // 创建 Websocket 对象，监听消息
+            // 登录
 
-            // 收到消息：检查新签到活动
+            try
+            {
+                CxSignClient client = null;
+                if (string.IsNullOrEmpty(AppConfig.Fid))
+                    client = await CxSignClient.LoginAsync(AppConfig.Username, AppConfig.Password);
+                else
+                    client = await CxSignClient.LoginAsync(AppConfig.Username, AppConfig.Password, AppConfig.Fid);
 
-            // 收到签到活动：执行签到流程
+                var imParams = await client.GetImTokenAsync();
 
-            return Task.FromResult(0);
+                // 创建 Websocket 对象，监听消息
+
+                var exitEvent = new ManualResetEvent(false);
+                var url = new Uri("wss://im-api-vip6-v2.easemob.com/ws/032/xvrhfd2j/websocket");
+                #region 消息处理
+                using (var wsClient = new WebsocketClient(url))
+                {
+                    wsClient.ReconnectionHappened.Subscribe(info =>
+                       Console.WriteLine($"Reconnection happened, type: {info.Type}"));
+
+                    wsClient.MessageReceived.Subscribe(msg =>
+                    {
+                        Console.WriteLine($"Message received: {msg}");
+                        if (msg.Text.StartsWith("o"))
+                        {
+                            var loginPackage = cxim.BuildLoginPackage(imParams.TUid, imParams.ImToken);
+                            Console.WriteLine($"Message send: {loginPackage}");
+                            wsClient.Send(loginPackage);
+                            return;
+                        }
+
+                        if (msg.Text.StartsWith("a"))
+                        {
+                            var json = JArray.Parse(msg.Text.Substring(1));
+                            var pkgBytes = Convert.FromBase64String(json[0].Value<string>());
+                            if (pkgBytes.Length <= 5) return;
+                            var t = new byte[5];
+                            Array.Copy(pkgBytes, t, 5);
+                            if (t.SequenceEqual(new byte[] { 0x08, 0x00, 0x40, 0x02, 0x4a }))
+                            {
+                                var lenByte = new byte[1];
+                                Array.Copy(pkgBytes, 9, lenByte, 0, 1);
+                                var len = Convert.ToUInt32(lenByte[0]);
+                                var cid = new byte[len];
+                                Array.Copy(pkgBytes, 10, cid, 0, len);
+                                var cidStr = Encoding.ASCII.GetString(cid);
+                                Console.WriteLine($"收到来自 {cidStr} 的消息");
+                                // TODO: Sign
+                            }
+                        }
+
+                    });
+
+                    await wsClient.Start();
+
+                    exitEvent.WaitOne();
+                }
+                #endregion
+                Console.ReadKey();
+
+                // 收到消息：检查新签到活动
+
+                // 收到签到活动：执行签到流程
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return await base.OnExecuteAsync(app);
         }
     }
-
 }
---
+++ cx-auto-sign/cxim.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace cx_auto_sign
+{
+    public static class cxim
+    {
+        public static string BuildLoginPackage(string TUid, string ImToken)
+        {
+            var timestamp = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            MemoryStream s = new MemoryStream();
+            var bw = new BinaryWriter(s);
+            bw.Write(new byte[] { 0x08, 0x00, 0x12, 0x3c, 0x0a, 0x0e });
+            bw.Write(Encoding.ASCII.GetBytes("cx-dev#cxstudy"));
+            bw.Write(new byte[] { 0x12, 0x08 });
+            bw.Write(Encoding.ASCII.GetBytes(TUid));
+            bw.Write(new byte[] { 0x1a, 0x0b });
+            bw.Write(Encoding.ASCII.GetBytes("easemob.com"));
+            bw.Write(new byte[] { 0x22, 0x13 });
+            bw.Write(Encoding.ASCII.GetBytes($"webim_{timestamp}"));
+            bw.Write(new byte[] { 0x1a, 0x85, 0x01 });
+            bw.Write(Encoding.ASCII.GetBytes("$t$"));
+            bw.Write(Encoding.ASCII.GetBytes($"{ImToken}"));
+            bw.Write(new byte[] { 0x40, 0x03, 0x4a, 0xc0, 0x01, 0x08, 0x10, 0x12, 0x05, 0x33, 0x2e, 0x30, 0x2e, 0x30, 0x28, 0x00, 0x30, 0x00, 0x4a, 0x0d });
+            bw.Write(Encoding.ASCII.GetBytes($"{timestamp}"));
+            bw.Write(new byte[] { 0x62, 0x05, 0x77, 0x65, 0x62, 0x69, 0x6d, 0x6a, 0x13, 0x77, 0x65, 0x62, 0x69, 0x6d, 0x5f });
+            bw.Write(Encoding.ASCII.GetBytes($"{timestamp}"));
+            bw.Write(new byte[] { 0x72, 0x85, 0x01, 0x24, 0x74, 0x24 });
+            bw.Write(Encoding.ASCII.GetBytes($"{ImToken}"));
+            bw.Write(new byte[] { 0x50, 0x00, 0x58, 0x00 });
+            return $"[\"{Convert.ToBase64String(s.ToArray())}\"]";
+        }
+    }
+}
