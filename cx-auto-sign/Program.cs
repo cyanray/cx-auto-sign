f304d851492c18c3b52a71583e16022f76279644
cyanray <a484599279@hotmail.com>
2020-08-30 14:35:37 +0800

    新增: 检查最新版本功能;

--- cx-auto-sign/Program.cs
+++ cx-auto-sign/Program.cs
@@ -1,5 +1,13 @@
 ﻿using CxSignHelper;
 using McMaster.Extensions.CommandLineUtils;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using Serilog;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,8 +15,8 @@ using System.Threading.Tasks;
 namespace cx_auto_sign
 {
     [Command(
-        Name = "cx-auto-sign", 
-        Description = "超星自动签到工具", 
+        Name = "cx-auto-sign",
+        Description = "超星自动签到工具",
         ExtendedHelpText = @"
 提示:
   本程序采用 MIT 协议开源(https://github.com/cyanray/cx-auto-sign).
@@ -21,7 +29,37 @@ namespace cx_auto_sign
         )]
     class Program : CommandBase
     {
-        static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+        static async Task<int> Main(string[] args)
+        {
+            await CheckUpdate();
+            return await CommandLineApplication.ExecuteAsync<Program>(args);
+        }
+
+        private static async Task CheckUpdate()
+        {
+            if (File.Exists(".noupdate"))
+            {
+                Console.WriteLine("已跳过检查更新");
+                return;
+            }
+            try
+            {
+                Console.WriteLine("正在检查更新...");
+                var body = await GetLastestVersion();
+                if (!body.Version.Contains(GetVersion()))
+                {
+                    Console.WriteLine($"发现新版本: {body.Version}");
+                    Console.WriteLine(body.Info);
+                    Console.WriteLine("请前往 https://github.com/cyanray/cx-auto-sign/releases 下载更新，或者按任意键继续...");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("获取版本信息失败，请访问 https://github.com/cyanray/cx-auto-sign/releases 检查是否有更新");
+            }
+        }
 
         private static string GetVersion()
             => typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
@@ -31,6 +69,25 @@ namespace cx_auto_sign
             app.ShowHelp();
             return await base.OnExecuteAsync(app);
         }
+
+        private static async Task<(string Version, string Info)> GetLastestVersion()
+        {
+            RestClient client = new RestClient($"https://api.github.com/repos/cyanray/cx-auto-sign/releases/latest");
+            var request = new RestRequest(Method.GET);
+            var response = await client.ExecuteGetAsync(request);
+            var json = JObject.Parse(response.Content);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                string message = string.Empty;
+                if (json.ContainsKey("message"))
+                    message = json["message"].Value<string>();
+                throw new Exception($"获取最新版本失败: {message}");
+            }
+            var version = json["tag_name"].Value<string>();
+            var info = json["body"].Value<string>();
+            return (version, info);
+        }
+
     }
 
 }
