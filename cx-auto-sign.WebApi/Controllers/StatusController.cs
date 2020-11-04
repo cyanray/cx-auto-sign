2338fcafb8f6e6b1fbace78521901ce9436bef5e
cyanray <a484599279@hotmail.com>
2020-11-04 17:16:10 +0800

    增加: 初始化WebApi;

---
+++ cx-auto-sign.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cx_auto_sign.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var status = new Status()
+            {
+                Username = "631805010409",
+                CxAutoSignEnabled = false
+            };
+            return new JsonResult(status);
+        }
+    }
+}
---
+++ cx-auto-sign.WebApi/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cx_auto_sign.WebApi
+{
+    public class Status
+    {
+        public string Username { get; set; }
+        public bool CxAutoSignEnabled { get; set; }
+    }
+}
